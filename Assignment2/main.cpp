5f3e785063847c03772f845221577e9bf64ad3bc
Ahmed <ahmedmahmoudkzm@gmail.com>
2023-11-10 19:10:12 +0200

    Initial Commit

---
+++ Assignment2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Assignment2
+//
+//  Created by Ahmed Mahmoud on 10/11/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
